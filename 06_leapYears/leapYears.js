0ed27df6681001a1afbbcc1413d75f9fb19e195c
arpit mishra <arpit.mishra.out@gmail.com>
2024-07-03 17:43:08 +0530

    leap year problem

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,24 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    // checking if a year is century year or not 
+    let flag = true;
+    if(year % 100 === 0){
+        if(year % 400 === 0){
+            return flag;
+        }
+        else{
+            flag = false;
+            return flag;
+        }
+    }
+    else if(year % 4 === 0){
+        return flag;
+    }
+    else {
+        flag = false;
+        return flag;
+    }
 
+    return flag;
 };
 
 // Do not edit below this line
