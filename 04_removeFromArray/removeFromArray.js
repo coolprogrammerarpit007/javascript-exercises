fbb181e94976b430d080e1052f1ac19318335642
arpit mishra <arpit.mishra.out@gmail.com>
2024-07-03 15:55:00 +0530

    remove elem from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,19 +1,26 @@
 const removeFromArray = function(arr,...nums) {
     // Algo to remove elements from the array
     nums.forEach((num,i)=>{
+        // counting the number of occurances
+        const count = arr.filter((number) => number === num ).length;
         let index;
         index = arr.indexOf(num);
         if(index<0){
             return;
         }
-        arr.splice(index,1);
+        arr.splice(index,count);
 
     })
-    console.log(arr);
+    return arr;
 };
 
+// removeFromArray([1,2,3,4],9);
+// removeFromArray([1,2,3,4],9,2);
+// removeFromArray([1,2,3,4],1,2,3,4);
 // removeFromArray([1,2,3,4],3);
 // removeFromArray([1,2,3,4],3,2);
-removeFromArray([1,2,3,4],7,9);
+// removeFromArray([1,2,3,4],7,9);
+// removeFromArray([2,2,3,4],2);
+// removeFromArray([2,2,3,4],1,2,3,4);
 // Do not edit below this line
 module.exports = removeFromArray;
