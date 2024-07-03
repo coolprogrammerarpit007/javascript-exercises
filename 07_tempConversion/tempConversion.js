f20b88787a28dd9e5883852f6204b461120025c2
arpit mishra <arpit.mishra.out@gmail.com>
2024-07-03 19:21:41 +0530

    temperature conversion challenge

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+
+  let degreeCelcius = ((temp - 32) * (5/9));
+  // if conversion is negative then convert it to absolute
+  
+    degreeCelcius = Number(degreeCelcius.toFixed(1));
+  
+  return degreeCelcius;
+  
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  const degreeFarhenite = Number(((temp * (9/5)) + 32).toFixed(1));
+  return degreeFarhenite;
 };
 
 // Do not edit below this line
