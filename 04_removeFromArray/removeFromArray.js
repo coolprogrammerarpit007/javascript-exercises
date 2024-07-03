a216e1e5700fb2b28d06f21eef01fdc9dba416a5
arpit mishra <arpit.mishra.out@gmail.com>
2024-07-03 15:25:18 +0530

    solve the hello world,repeat string and reverse string problems

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr,...nums) {
+    // Algo to remove elements from the array
+    nums.forEach((num,i)=>{
+        let index;
+        index = arr.indexOf(num);
+        if(index<0){
+            return;
+        }
+        arr.splice(index,1);
+
+    })
+    console.log(arr);
 };
 
+// removeFromArray([1,2,3,4],3);
+// removeFromArray([1,2,3,4],3,2);
+removeFromArray([1,2,3,4],7,9);
 // Do not edit below this line
 module.exports = removeFromArray;
