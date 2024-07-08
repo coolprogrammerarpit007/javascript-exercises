6aa89e52ae5b2888b117d179e5af42d28d864c05
arpit mishra <arpit.mishra.out@gmail.com>
2024-07-08 12:14:06 +0530

    palindrome string

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,22 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str.toLowerCase().replace(/[.,\/#!$%\^&\s+\*;:{}=\-_`~()]/g,"");
+    revseseStr = ``;
+    let flag = false;
+    for(let i=str.length-1;i>=0;i--){
+        revseseStr += str[i];
+    }
 
+    if(str === revseseStr){
+        flag = true;
+    }
+
+
+    // console.log(str);
+    // console.log(revseseStr);
+    return flag;
 };
+
+// palindromes('A car, a man, a maraca.');
 
 // Do not edit below this line
 module.exports = palindromes;
