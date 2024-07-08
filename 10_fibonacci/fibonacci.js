791885c76d9b929d37f9a870bcf6beff2e31045c
arpit mishra <arpit.mishra.out@gmail.com>
2024-07-08 17:43:39 +0530

    fibonacci series

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = Number(num);
+    
+    let num1 = 0;
+    let num2 = 1;
+    let total = 0;
 
+    if(num < 0){
+        total = 'OOPS';
+    }
+    if(num === 1){
+        total = 1;
+    }
+
+    for(let i=2;i<=num;i++){
+        total = num1 + num2;
+        num1 =  num2;
+        num2 = total;
+    }
+    return total;
 };
-
+fibonacci(2);
 // Do not edit below this line
 module.exports = fibonacci;
